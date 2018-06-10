231e12deef4b935848f057a91fed6108482890eb
Analia Mok <axm2580@g.rit.edu>
2018-06-10 12:50:09 -0400

    Added responsiveness to content and footer. Not nav

--- resources/views/pages/page.blade.php
+++ resources/views/pages/page.blade.php
@@ -25,49 +25,48 @@
 @endsection
 
 @section('content')
-@if(!$posts)
-  {{-- Just page content --}}
-  {!! $page->general_content !!}
-@elseif(count($posts) > 0)
-<div class="page">
-  <ul class="page__post-content">
-    @foreach ($posts as $post)
-    <li class="page__post-content__post">
-      <div class="page__post-content__post__image-wrapper">
-        <a href="/{{ $slug_base }}/{{ $post->slug }}">
-          <img src="{{ Voyager::image($post->thumbnail('small')) }}" alt="{{ $post->title ?? $post->name }}">
-        </a>
-      </div>
-      <a href="/{{ $slug_base }}/{{ $post->slug }}">
-        <div class="page__post-content__post__content">
-          @if( isset($post->categories))
-            @foreach( $post->categories as $category )
-              <span class="category">{{ $category->name }}</span>
-            @endforeach
-          @endif
-          <h3>{{ $post->title ?? $post->name }}</h3>
-          @if( $post->author )
-            <em>Written by {{ $post->author->name }}</em>
-          @endif
-          <div class="page__post-content__post__content__bottom">
-            @if($post->tutorials)
-            <span class="page__post-content__post__content__bottom__total">{{ count($post->tutorials) }} Tutorials</span>
-            @endif
-            <span class="page__post-content__post__content__bottom__date">
-              @php
-              echo date('M d, Y', strtotime($post->updated_at))
-              @endphp
-            </span>
-          </div>
+  @if(!$posts)
+    {{-- Just page content --}}
+    {!! $page->general_content !!}
+  @elseif(count($posts) > 0)
+  <div class="page">
+    <ul class="page__post-content">
+      @foreach ($posts as $post)
+      <li class="page__post-content__post">
+        <div class="page__post-content__post__image-wrapper">
+          <a href="/{{ $slug_base }}/{{ $post->slug }}">
+            <img src="{{ Voyager::image($post->thumbnail('small')) }}" alt="{{ $post->title ?? $post->name }}">
+          </a>
         </div>
-      </a>
-    </li>
-    @endforeach
-  </ul>
-  {{ $posts->links() }}
-</div>
-@else
-  <strong>Sorry, there are no available posts</strong>
-@endif
-
+        <a href="/{{ $slug_base }}/{{ $post->slug }}">
+          <div class="page__post-content__post__content">
+            @if( isset($post->categories))
+              @foreach( $post->categories as $category )
+                <span class="category">{{ $category->name }}</span>
+              @endforeach
+            @endif
+            <h3>{{ $post->title ?? $post->name }}</h3>
+            @if( $post->author )
+              <em>Written by {{ $post->author->name }}</em>
+            @endif
+            <div class="page__post-content__post__content__bottom">
+              @if($post->tutorials)
+              <span class="page__post-content__post__content__bottom__total">{{ count($post->tutorials) }} Tutorials</span>
+              @endif
+              <span class="page__post-content__post__content__bottom__date">
+                @php
+                echo date('M d, Y', strtotime($post->updated_at))
+                @endphp
+              </span>
+            </div>
+          </div>
+        </a>
+      </li>
+      @endforeach
+    </ul>
+    {{ $posts->links() }}
+  </div>
+  @else
+    <strong>Sorry, there are no available posts</strong>
+  @endif
 @endsection
