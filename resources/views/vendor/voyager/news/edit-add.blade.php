63b5c12206150842df9236990efd6475e72ea707
Analia Mok <axm2580@g.rit.edu>
2018-06-13 19:52:38 -0400

    Started customization of news edit screen

--- resources/views/vendor/voyager/news/edit-add.blade.php
+++ resources/views/vendor/voyager/news/edit-add.blade.php
@@ -15,114 +15,130 @@
 @stop
 
 @section('content')
-    <div class="page-content edit-add container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-
-                <div class="panel panel-bordered">
-                    <!-- form start -->
-                    <form role="form"
-                            class="form-edit-add"
-                            action="@if(!is_null($dataTypeContent->getKey())){{ route('voyager.'.$dataType->slug.'.update', $dataTypeContent->getKey()) }}@else{{ route('voyager.'.$dataType->slug.'.store') }}@endif"
-                            method="POST" enctype="multipart/form-data">
-                        <!-- PUT Method if we are editing -->
-                        @if(!is_null($dataTypeContent->getKey()))
-                            {{ method_field("PUT") }}
-                        @endif
-
-                        <!-- CSRF TOKEN -->
-                        {{ csrf_field() }}
-
-                        <div class="panel-body">
-
-                            @if (count($errors) > 0)
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-
-                            <!-- Adding / Editing -->
-                            @php
-                                $dataTypeRows = $dataType->{(!is_null($dataTypeContent->getKey()) ? 'editRows' : 'addRows' )};
-                            @endphp
-
-                            @foreach($dataTypeRows as $row)
-                                <!-- GET THE DISPLAY OPTIONS -->
-                                @php
-                                    $options = json_decode($row->details);
-                                    $display_options = isset($options->display) ? $options->display : NULL;
-                                @endphp
-                                @if ($options && isset($options->legend) && isset($options->legend->text))
-                                    <legend class="text-{{$options->legend->align or 'center'}}" style="background-color: {{$options->legend->bgcolor or '#f0f0f0'}};padding: 5px;">{{$options->legend->text}}</legend>
-                                @endif
-                                @if ($options && isset($options->formfields_custom))
-                                    @include('voyager::formfields.custom.' . $options->formfields_custom)
-                                @else
-                                    <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width or 12 }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
-                                        {{ $row->slugify }}
-                                        <label for="name">{{ $row->display_name }}</label>
-                                        @include('voyager::multilingual.input-hidden-bread-edit-add')
-                                        @if($row->type == 'relationship')
-                                            @include('voyager::formfields.relationship')
-                                        @else
-                                            {!! app('voyager')->formField($row, $dataType, $dataTypeContent) !!}
-                                        @endif
-
-                                        @foreach (app('voyager')->afterFormFields($row, $dataType, $dataTypeContent) as $after)
-                                            {!! $after->handle($row, $dataType, $dataTypeContent) !!}
-                                        @endforeach
-                                    </div>
-                                @endif
-                            @endforeach
-
-                        </div><!-- panel-body -->
-
-                        <div class="panel-footer">
-                            <button type="submit" class="btn btn-primary save">{{ __('voyager::generic.save') }}</button>
-                        </div>
-                    </form>
-
-                    <iframe id="form_target" name="form_target" style="display:none"></iframe>
-                    <form id="my_form" action="{{ route('voyager.upload') }}" target="form_target" method="post"
-                            enctype="multipart/form-data" style="width:0;height:0;overflow:hidden">
-                        <input name="image" id="upload_file" type="file"
-                                 onchange="$('#my_form').submit();this.value='';">
-                        <input type="hidden" name="type_slug" id="type_slug" value="{{ $dataType->slug }}">
-                        {{ csrf_field() }}
-                    </form>
-
-                </div>
+  <div class="page-content edit-add container-fluid">
+    <form role="form"
+      class="form-edit-add"
+      action="@if(!is_null($dataTypeContent->getKey())){{ route('voyager.'.$dataType->slug.'.update', $dataTypeContent->getKey()) }}@else{{ route('voyager.'.$dataType->slug.'.store') }}@endif"
+      method="POST" enctype="multipart/form-data">
+      <!-- PUT Method if we are editing -->
+      @if(!is_null($dataTypeContent->getKey()))
+          {{ method_field("PUT") }}
+      @endif
+      
+      <!-- CSRF TOKEN -->
+      {{ csrf_field() }}
+            
+      <div class="row">
+        <div class="col-md-8">
+          <!-- Title -->            
+          <div class="panel">
+            <div class="panel-heading">
+              <h3 class="panel-title">
+                <span class="panel-desc">Title</span>
+              </h3>
+              <div class="panel-actions">
+                <a class="panel-action voyager-angle-down" data-toggle="panel-collapse" aria-hidden="true"></a>
+              </div>
             </div>
-        </div>
-    </div>
-
-    <div class="modal fade modal-danger" id="confirm_delete_modal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"
-                            aria-hidden="true">&times;</button>
-                    <h4 class="modal-title"><i class="voyager-warning"></i> {{ __('voyager::generic.are_you_sure') }}</h4>
-                </div>
-
-                <div class="modal-body">
-                    <h4>{{ __('voyager::generic.are_you_sure_delete') }} '<span class="confirm_delete_name"></span>'</h4>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
-                    <button type="button" class="btn btn-danger" id="confirm_delete">{{ __('voyager::generic.delete_confirm') }}
-                    </button>
-                </div>
+            <div class="panel-body">
+              <input type="text" class="form-control" id="title" name="title" placeholder="title" value="@if(isset($dataTypeContent->title)){{ $dataTypeContent->title }}@endif"/>
             </div>
+          </div>
+          <div class="panel">
+
+              <div class="panel-body">
+
+                  @if (count($errors) > 0)
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->all() as $error)
+                                  <li>{{ $error }}</li>
+                              @endforeach
+                          </ul>
+                      </div>
+                  @endif
+
+                  <!-- Adding / Editing -->
+                  @php
+                      $dataTypeRows = $dataType->{(!is_null($dataTypeContent->getKey()) ? 'editRows' : 'addRows' )};
+                      $exclude = ['title'];
+                  @endphp
+
+                  @foreach($dataTypeRows as $row)
+                      @if(!in_array($row->field, $exclude))
+                          <!-- GET THE DISPLAY OPTIONS -->
+                          @php
+                              $options = json_decode($row->details);
+                              $display_options = isset($options->display) ? $options->display : NULL;
+                          @endphp
+                          @if ($options && isset($options->legend) && isset($options->legend->text))
+                              <legend class="text-{{$options->legend->align or 'center'}}" style="background-color: {{$options->legend->bgcolor or '#f0f0f0'}};padding: 5px;">{{$options->legend->text}}</legend>
+                          @endif
+                          @if ($options && isset($options->formfields_custom))
+                              @include('voyager::formfields.custom.' . $options->formfields_custom)
+                          @else
+                              <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width or 12 }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
+                                  {{ $row->slugify }}
+                                  <label for="name">{{ $row->display_name }}</label>
+                                  @include('voyager::multilingual.input-hidden-bread-edit-add')
+                                  @if($row->type == 'relationship')
+                                      @include('voyager::formfields.relationship')
+                                  @else
+                                      {!! app('voyager')->formField($row, $dataType, $dataTypeContent) !!}
+                                  @endif
+
+                                  @foreach (app('voyager')->afterFormFields($row, $dataType, $dataTypeContent) as $after)
+                                      {!! $after->handle($row, $dataType, $dataTypeContent) !!}
+                                  @endforeach
+                              </div>
+                          @endif
+                      @endif
+                  @endforeach
+
+              </div><!-- panel-body -->
+
+              <div class="panel-footer">
+                  <button type="submit" class="btn btn-primary save">{{ __('voyager::generic.save') }}</button>
+              </div>
+              
+          </div>
         </div>
-    </div>
-    <!-- End Delete File Modal -->
+      </div>
+    </form>
+
+    <iframe id="form_target" name="form_target" style="display:none"></iframe>
+    <form id="my_form" action="{{ route('voyager.upload') }}" target="form_target" method="post"
+            enctype="multipart/form-data" style="width:0;height:0;overflow:hidden">
+        <input name="image" id="upload_file" type="file"
+                  onchange="$('#my_form').submit();this.value='';">
+        <input type="hidden" name="type_slug" id="type_slug" value="{{ $dataType->slug }}">
+        {{ csrf_field() }}
+    </form>
+  </div>
+
+  <div class="modal fade modal-danger" id="confirm_delete_modal">
+      <div class="modal-dialog">
+          <div class="modal-content">
+
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal"
+                          aria-hidden="true">&times;</button>
+                  <h4 class="modal-title"><i class="voyager-warning"></i> {{ __('voyager::generic.are_you_sure') }}</h4>
+              </div>
+
+              <div class="modal-body">
+                  <h4>{{ __('voyager::generic.are_you_sure_delete') }} '<span class="confirm_delete_name"></span>'</h4>
+              </div>
+
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
+                  <button type="button" class="btn btn-danger" id="confirm_delete">{{ __('voyager::generic.delete_confirm') }}
+                  </button>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- End Delete File Modal -->
 @stop
 
 @section('javascript')
