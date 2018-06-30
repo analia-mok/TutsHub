90b86fb2c14656e88a3aec0110617bc9cd7e08ec
Analia Mok <axm2580@g.rit.edu>
2018-06-30 10:21:56 -0400

    PagesController now PSR2 compatabile

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,110 +7,115 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 
-class PagesController extends Controller {
+class PagesController extends Controller
+{
 
-  /**
-   * Retrieves and returns a page object and view
-   *
-   * Generalized method to help account for pages created in the future
-   * @param string $slug
-   * @return void
-   */
-  public function index($slug = ""){
-    
-    if(empty($slug)) {
-      // Empty slug indicated just a slug of slash
-      // Direct to home page
-      $slug = 'home';
+    /**
+     * Retrieves and returns a page object and view
+     *
+     * Generalized method to help account for pages created in the future
+     * @param string $slug
+     * @return void
+     */
+    public function index($slug = "")
+    {
+
+        if (empty($slug)) {
+            // Empty slug indicated just a slug of slash
+            // Direct to home page
+            $slug = 'home';
+        }
+
+        $page = \App\Page::where('slug', $slug)->first();
+
+        if ($page === null || ($page !== null && $page->status !== "PUBLISHED")) {
+            return abort(404);
+        }
+
+        $posts = null;
+        $view = 'pages.page'; // Default view
+        $slug_base = ''; // Base slug for each item link
+
+        switch ($slug) {
+            case 'news':
+                $posts = $this->getNews();
+                $slug_base = $slug;
+                break;
+            case 'guides':
+                $posts = $this->getGuides();
+                $slug_base = $slug;
+                break;
+            case 'tutorials':
+                $posts = $this->getTutorials();
+                $slug_base = $slug;
+                break;
+            case 'home':
+                $view = 'pages.home';
+                break;
+            case 'about':
+                $view = 'pages.about';
+                break;
+        }
+
+        $data = [
+            'page'      => $page,
+            'posts'     => $posts,
+            'slug_base' => $slug_base,
+        ];
+
+        return view($view)->with($data);
     }
 
-    $page = \App\Page::where('slug', $slug)->first();
-
-    if($page === null || ($page !== null && $page->status !== "PUBLISHED")){
-      return abort(404);
+    /**
+     * Retrieves all data related to the about page
+     *
+     * @return Array $data - All variables expected by the about page view
+     */
+    public function getAbouPageData()
+    {
+        // TODO
+        return [];
     }
 
-    $posts = null;
-    $view = 'pages.page'; // Default view
-    $slug_base = ''; // Base slug for each item link
-    
-    switch($slug) {
-      case 'news':
-        $posts = $this->getNews();
-        $slug_base = $slug;
-        break;
-      case 'guides':
-        $posts = $this->getGuides();
-        $slug_base = $slug;
-        break;
-      case 'tutorials':
-        $posts = $this->getTutorials();
-        $slug_base = $slug;
-        break;
-      case 'home':
-        $view = 'pages.home';
-        break;
-      case 'about':
-        $view = 'pages.about';
-        break;
+    /**
+     * Retreives published tutorials + pivot data
+     *
+     * @return Array $data
+     */
+    public function getTutorials()
+    {
+        $data = \App\Tutorial::where('status', 'PUBLISHED')
+                            ->orderBy('published_date', 'desc')
+                            ->paginate(12);
+
+        return $data;
     }
 
-    $data = [
-      'page'      => $page,
-      'posts'     => $posts,
-      'slug_base' => $slug_base,
-    ];
+    /**
+     * Retreives published guides + pivot data
+     *
+     * @return Array $data
+     */
+    public function getGuides()
+    {
+        $data = \App\Guide::where('status', 'PUBLISHED')
+                            ->orderBy('published_date', 'desc')
+                            ->paginate(12);
 
-    return view($view)->with($data);
-  }
+        return $data;
+    }
 
-  /**
-   * Retrieves all data related to the about page
-   *
-   * @return Array $data - All variables expected by the about page view
-   */
-  public function getAbouPageData(){
-    // TODO
-    return [];
-  }
+    /**
+     * Retreives published tutorials + pivot data
+     *
+     * @return Array $data
+     */
+    public function getNews()
+    {
+        $data = \App\News::where('status', 'PUBLISHED')
+                            ->orderBy('published_date', 'desc')
+                            ->paginate(12);
 
-  /**
-   * Retreives published tutorials + pivot data
-   *
-   * @return Array $data
-   */
-  public function getTutorials() {
-    $data = \App\Tutorial::where('status', 'PUBLISHED')
-                        ->orderBy('published_date', 'desc')
-                        ->paginate(12);
-
-    return $data;
-  }
-  
-  /**
-   * Retreives published guides + pivot data
-   *
-   * @return Array $data
-   */
-  public function getGuides() {
-    $data = \App\Guide::where('status', 'PUBLISHED')
-                      ->orderBy('published_date', 'desc')
-                      ->paginate(12);
-
-    return $data;
-  }
-  
-  /**
-   * Retreives published tutorials + pivot data
-   *
-   * @return Array $data
-   */
-  public function getNews() {
-    $data = \App\News::where('status', 'PUBLISHED')
-                      ->orderBy('published_date', 'desc')
-                      ->paginate(12);
-
-    return $data;
-  }
-
+        return $data;
+    }
 }
